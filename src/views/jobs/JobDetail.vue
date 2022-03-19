a230e1ef648f0db2215f96b5e163a75c0faacb88
Dice1111 <kingdice1111@gmail.com>
2022-03-19 16:12:32 +0630

    Linking

---
+++ src/views/jobs/JobDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <H1>Job Detail {{id}}</H1>
+</template>
+
+<script>
+export default {
+  // data(){
+  //   return{
+  //     id:this.$route.params.id
+  //   }
+  // }
+  props:['id']
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 style="color:crimson">Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <router-link :to="{name:'jobdetail',params:{id:job.id}}">
+    <h1>{{job.title}}</h1>
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs:[
+                {id:"1",title:"project manager",detail:"apple manager"},
+                {id:"2",title:"project designer",detail:"apple designer"},
+                {id:"3",title:"project developer",detail:"apple developer"}
+
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
